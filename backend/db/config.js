ea65d5a7ea5bd35e064ad5aa7a771d6a97b79307
VishalManani <vishalmanani35@gmail.com>
2024-10-29 06:54:21 +0530

    Database connected

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/e-dashboard");
-mongoose.connect("mongodb://127.0.0.1/e-dashboard");
+
+// mongoose.connect("mongodb://127.0.0.1/e-dashboard");
+const DB = process.env.MONGODB_URL
+mongoose.connect(DB,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}).then(()=> console.log("DataBase Connected")).catch((err)=>{
+    console.log(err);
+})
